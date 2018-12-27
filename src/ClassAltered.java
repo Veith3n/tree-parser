00b399158e2823861320e3f2fcf8b533620447c0
Krzysztof Kopydłowski <veithen123@gmail.com>
2018-12-27 17:47:33 +0100

    implementation of initializing numeric attributes

---
+++ src/ClassAltered.java
@@ -0,0 +1,18 @@
+public final class ClassAltered {
+
+    private Integer integerClass = 0;
+
+    private int integerPrimitve = 0;
+
+    private Float floatClass = 0f;
+
+    private float floatPrimitive = 0f;
+
+    private Double doubleClass = 0d;
+
+    private double doublePrimitive = 0d;
+
+    private long primitiveLong = 0L;
+
+    private Long classLong = 0L;
+}
--- src/pl/edu/wat/Main.java
+++ src/pl/edu/wat/Main.java
@@ -2,6 +2,7 @@ package pl.edu.wat;
 
 import com.github.javaparser.JavaParser;
 import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.FieldDeclaration;
 
 import javax.tools.DiagnosticCollector;
 import javax.tools.JavaCompiler;
@@ -13,8 +14,12 @@ import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.List;
 
-public class Main { 
+public class Main {
+
+  private static final List<String> NUMERIC_TYPES = Arrays.asList("long", "int", "integer", "double", "float");
+
   public static void main(String[] args) throws IOException {
     final String fileName = "src/Class.java";
     final String alteredFileName = "src/ClassAltered.java";
@@ -22,6 +27,9 @@ public class Main {
     try (FileInputStream in = new FileInputStream(fileName)) {
       cu = JavaParser.parse(in);
     }
+
+    List<FieldDeclaration> attributes = cu.getChildNodesByType(FieldDeclaration.class);
+    initializeNumericAttributes(attributes);
 
     cu.getClassByName("Class").get().setName("ClassAltered");
 
@@ -41,5 +49,42 @@ public class Main {
 
       diagnostics.getDiagnostics().forEach(d -> System.out.println(d.getMessage(null)));
     }
+  }
+
+  private static void initializeNumericAttributes(List<FieldDeclaration> attributes) {
+    attributes.stream().
+        filter(attribute -> NUMERIC_TYPES.contains(getAttributeName(attribute))).
+        forEach(Main::initializeAttribute);
+  }
+
+  private static void initializeAttribute(FieldDeclaration attribute) {
+    if (!isAttributeInitialized(attribute)) {
+      attribute.getVariables().get(0).setInitializer(generateValue(attribute));
+    }
+  }
+
+  private static boolean isAttributeInitialized(FieldDeclaration node) {
+    return node.getVariables().get(0).getInitializer().isPresent();
+  }
+
+  private static String generateValue(FieldDeclaration attribute) {
+    return needLetterLiteral(getAttributeName(attribute)) ? ("0" + letterLiteral(getFirstLetter(attribute))) : "0";
+  }
+
+  private static boolean needLetterLiteral(String name) {
+    List<String> whitelist = Arrays.asList("float", "double", "long");
+    return whitelist.contains(name);
+  }
+
+  private static String letterLiteral(String letter) {
+    return letter.equals("l") ? "L" : letter;
+  }
+
+  private static String getFirstLetter(FieldDeclaration attribute) {
+    return getAttributeName(attribute).substring(0, 1);
+  }
+
+  private static String getAttributeName(FieldDeclaration attribute) {
+    return attribute.getCommonType().toString().toLowerCase();
   }
 }
